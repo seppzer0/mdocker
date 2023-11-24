4b27c50d9b28ae28137671a3dbaecf9f330f50a3
seppzer0 <s3ppzer0@gmail.com>
2023-11-24 17:19:48 +0200

    scripts: replace a Python script with Shell script

--- scripts/get_version.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-
-
-apath = Path(Path(__file__).absolute().parents[1])
-with open(Path(apath, "pyproject.toml")) as f:
-    print(f.read().split('version = "')[1].split('"')[0])
